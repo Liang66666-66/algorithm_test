d18d1c941f58b7862b3d75dbc3900f20f0894b9b
Liang66666-66 <18862308075@163.com>
2025-12-27 16:52:18 +0800

    新增栈和队列

--- data_structure/stack.py
+++ data_structure/stack.py
@@ -1,4 +1,11 @@
+#栈
+"""
+只能从队尾进入或输出的列表
+"""
 def kuohaomatch(my_str):
+    """
+    使用栈解决括号字符串匹配问题
+    """
     stk=[]
     my_dict={'{':'}','[':']','(':')'}
     for i in my_str:
@@ -18,8 +25,6 @@ def kuohaomatch(my_str):
     else:
         return False
 
-
-
 the_str='{[(){}]}'
 false_str='{[}]'
 o_false_str='{'
@@ -29,3 +34,8 @@ print(kuohaomatch(the_str))
 print(kuohaomatch(false_str))
 print(kuohaomatch(o_false_str))
 print(kuohaomatch(o1_false_str))
+
+
+
+        
+
